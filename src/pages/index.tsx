7cf7dd9f4705b32cd982eba99707e282f7bfe38e
Willian S <wsapcf@gmail.com>
2021-06-08 08:34:10 -0300

    add roboto fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/home.module.scss";
 //cada arquivo dentro da pasta pages se torna uma rota do site
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>ig.news</title>
+      </Head>
       <h1 className={styles.title}>Hello!</h1>
     </div>
   );
