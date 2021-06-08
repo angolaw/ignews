901428797ef7b0bc4f1db996e9a5aacd717448f6
Willian S <wsapcf@gmail.com>
2021-06-08 08:35:08 -0300

    add fragment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,14 @@ import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello!</h1>
-    </div>
+
+      <div>
+        <h1 className={styles.title}>Hello!</h1>
+      </div>
+    </>
   );
 }
