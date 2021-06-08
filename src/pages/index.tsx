9a56eee0436e7c675f0f2aa41c0a63b0951588a3
Willian S <wsapcf@gmail.com>
2021-06-08 09:14:37 -0300

    header first half

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import { Header } from "../components/Header";
-import styles from "../styles/home.module.scss";
 //cada arquivo dentro da pasta pages se torna uma rota do site
 
 export default function Home() {
@@ -10,9 +8,7 @@ export default function Home() {
         <title>ig.news</title>
       </Head>
 
-      <div>
-        <Header />
-      </div>
+      <div></div>
     </>
   );
 }
